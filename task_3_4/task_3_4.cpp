a8341287e3e7b79d1637d833e0a3fe5f675b1515
VadimContentHunter <project.k.vadim@gmail.com>
2024-09-29 13:33:32 +0300

    Completed task 3.3

---
+++ task_3_4/task_3_4.cpp
@@ -0,0 +1,37 @@
+﻿// task_3_4.cpp :
+// Написать программу, которая выводит все простые числа от 1 до 100.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    locale::global(locale(""));
+
+    cout
+        << "\n-----------------------------------------------------------------------------------"
+        << "\nЗадание 3.4:"
+        << "Написать программу, которая выводит все простые числа от 1 до 100."
+        << endl;
+
+    int maxNum = 100;
+
+    for (int i = 2; i <= maxNum; i++)
+    {
+        int isPrimeNum = true;
+        for (int j = 2; j < i; j++)
+        {
+            if (i % j == 0) {
+                isPrimeNum = false;
+                break;
+            }
+        }
+        if (isPrimeNum) {
+            printf("%d - являеться простым числом\n", i);
+        }
+    }
+
+    return 0;
+}
